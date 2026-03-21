da631207a1a99706fab364d5889ffdb008cf6d95
Nastya Gr <proekt28n@gmail.com>
2026-03-22 00:21:25 +0500

    Напсание шагов тестирования ТК4, проверки до 2 шага

--- tests/test_filter_price.py
+++ tests/test_filter_price.py
@@ -12,10 +12,14 @@ def test_04_filter(page: Page):
     expect(page.locator('//label[contains(text(), "от ")]/input')).to_have_value('500')
 
     office.fill_filter_price_by_name('до ', '2000')
-
-    # expect(page.locator('input[name="s[price][max]"]')).to_have_value('2000')
+    expect(page.locator('//label[contains(text(), "до ")]/input')).to_have_value('2000')
     time.sleep(2)
-    # office.click_button_by_name('Показать')
-    # time.sleep(2)
+    office.click_button_by_name('Показать')
+    time.sleep(2)
+    count_product = page.locator(".product-top").count()
+    expect(page.locator(".filter-result")).to_be_visible()
+    expect(page.locator("#filter-result")).to_have_text(f"{count_product}")
+    expect(page.locator(".product-top")).to_have_count("5")
+
     # office.click_button_by_name('Сбросить фильтр')
     # time.sleep(2)
