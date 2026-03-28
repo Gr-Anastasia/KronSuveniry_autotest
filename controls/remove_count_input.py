3817fea47f28730303b74ec7e352da19c441a867
Nastya Gr <proekt28n@gmail.com>
2026-03-28 13:07:13 +0500

    Добавление контролов для кнопок + и - в количестве карточкци товара

---
+++ controls/remove_count_input.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page, Locator
+from controls.base_control import BaseControl
+
+class RemoveProductCount(BaseControl):
+    """
+    Класс, описывающий кнопку "-" в количестве в карточке товара на страницах разделов
+    """
+    def __init__(self, page: Page, wrapper: Locator):
+        super().__init__(page, wrapper)
