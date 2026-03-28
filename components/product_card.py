654ec2ab9fa64024818f7493e5a85f46759359ae
Nastya Gr <proekt28n@gmail.com>
2026-03-28 14:18:42 +0500

    Добавлеие методов с количествои в компонент карточки товара

--- components/product_card.py
+++ components/product_card.py
@@ -3,7 +3,9 @@ from components.base_component import BaseComponent
 from controls.input_all import InputAll
 from controls.button_all import ButtonAll
 from controls.checkbox_comparison import CheckboxComparison
-from controls.title_product import ProductTitle
+from controls.add_count_input import AddProductCount
+from controls.remove_count_input import RemoveProductCount
+
 
 class ProductCard(BaseComponent):
     """
@@ -13,5 +15,16 @@ class ProductCard(BaseComponent):
         super().__init__(page, wrapper)
 
     def get_button_in_product_by_name(self, name):
-        return ButtonAll(self.page, self.page.locator(f'.shop2-button-left:has-text("{name}")'))
+        return ButtonAll(self.page, self.wrapper.locator(f'.shop2-button-left:has-text("{name}")'))
+
+    def get_add_count_input(self):
+        return AddProductCount(self.page, self.wrapper.locator(".product-amount-button p-plus"))
+
+    def get_remove_count_input(self):
+        return RemoveProductCount(self.page, self.wrapper.locator(".product-amount-button p-minus"))
+
+    def get_input_count(self):
+        return InputAll(self.page, self.wrapper.locator('input[name="amount"]'))
+
+
 
