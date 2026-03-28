6d46c3e2cef30efa5c0c5a8ef735124153d3dfc9
Nastya Gr <proekt28n@gmail.com>
2026-03-28 14:19:07 +0500

    Добавлеие методов с количеством в старницу карточки товара

--- pages/product_page.py
+++ pages/product_page.py
@@ -14,3 +14,12 @@ class ProductPage(BasePage):
 
     def click_button_product_by_name(self, name):
         return self.get_product().get_button_in_product_by_name(name).wrapper.click()
+
+    def click_button_add_count_input_by_title(self):
+        return self.get_product().get_add_count_input().wrapper.click()
+
+    def click_button_remove_count_input_by_title(self):
+        return self.get_product().get_remove_count_input().wrapper.click()
+
+    def fill_input_count_by_title(self, fill_value):
+        return self.get_product().get_input_count().wrapper.fill(f"{fill_value}")
