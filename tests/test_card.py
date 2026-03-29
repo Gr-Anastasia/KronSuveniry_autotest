663061df479a766760982be1df4675e01ca66421
Nastya Gr <proekt28n@gmail.com>
2026-03-29 21:54:46 +0500

    Реализация ШТ 1-4 и ОР для ТК12

--- tests/test_card.py
+++ tests/test_card.py
@@ -6,6 +6,8 @@ from pages.product_page import ProductPage
 from pages.card_page import CardPage
 from playwright.sync_api import expect, Page
 
+from pages.section_page import SectionPage
+
 
 def test_09_card(page: Page):
     product = ProductPage(page, "https://pumpenergy.ru/catalog/pryanick-tula")
@@ -44,7 +46,31 @@ def test_10_from_card_to_product(page: Page):
     expect(page).to_have_url("https://pumpenergy.ru/catalog/pryanick-tula")
     expect(page.get_by_role("heading", name="Тульский пряник")).to_be_visible()
 
+def test_11_card_count(page: Page):
+    office = SectionPage(page, "https://pumpenergy.ru/catalog/office")
+    main = MainPage(page, "https://pumpenergy.ru/catalog/office")
+    card = CardPage(page, "https://pumpenergy.ru/catalog/cart")
+    office.open()
 
+    office.click_button_buy_in_product_card_by_title("Карта-флешка")
+    expect(page.locator(".added-to-cart:has-text('Добавлено')")).to_be_visible()
+
+    office.click_button_buy_in_product_card_by_title("Бумажный пакет")
+    expect(page.locator(".added-to-cart:has-text('Добавлено')")).to_be_visible()
+
+    main.click_to_cart()
+    expect(page).to_have_url("https://pumpenergy.ru/catalog/cart")
+    expect(page.get_by_role("heading", name="Корзина")).to_be_visible()
+    expect(page.get_by_role("link", name="Карта-флешка")).to_be_visible()
+    expect(page.get_by_role("link", name="Бумажный пакет")).to_be_visible()
+    # expect(page.locator('[id*="cart-row"]')).to_have_count(2)
+
+    card.fill_count_product_by_title_in_list_card("Карта-флешка", "22")
+    card.click_button_product_by_name("Пересчитать")
+    time.sleep(2)
+
+    card.click_button_product_by_name("Очистить корзину")
+    time.sleep(2)
 
 
 
