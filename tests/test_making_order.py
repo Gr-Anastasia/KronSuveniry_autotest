a2f9efc5e5d21984ffea499508e3792eb6130507
Nastya Gr <proekt28n@gmail.com>
2026-04-01 21:50:11 +0500

    Реализация ШТ для ТК14, добавила проверки для Тк13

--- tests/test_making_order.py
+++ tests/test_making_order.py
@@ -6,6 +6,7 @@ from pages.order_page import OrderPage
 from pages.card_page import CardPage
 from playwright.sync_api import expect, Page
 
+from pages.product_page import ProductPage
 from pages.section_page import SectionPage
 
 
@@ -99,6 +100,38 @@ def test_13_making_an_order_not_all_data(page: Page):
     # expect(page.get_by_label("E-mail")).to_have_text("qa@mail.com")
 
     time.sleep(2)
+
+
+def test_14_making_an_order_courier(page: Page):
+    pie = ProductPage(page, "https://pumpenergy.ru/catalog/pryanick-tula")
+    main = MainPage(page, "https://pumpenergy.ru/catalog/office")
+    card = CardPage(page, "https://pumpenergy.ru/catalog/cart")
+    order = OrderPage(page, "https://pumpenergy.ru/catalog?mode=order")
+    pie.open()
+
+    pie.click_button_product_by_name("Купить")
+    expect(page.locator(".added-to-cart:has-text('Добавлено')")).to_be_visible()
+
+    main.click_to_cart()
+    expect(page).to_have_url("https://pumpenergy.ru/catalog/cart")
+    expect(page.get_by_role("heading", name="Корзина")).to_be_visible()
+    expect(page.get_by_role("link", name="Тульский пряник")).to_be_visible()
+
+    card.click_button_buy_no_register()
+    expect(page).to_have_url("https://pumpenergy.ru/catalog?mode=order")
+    expect(page.get_by_role("heading", name="Доставка")).to_be_visible()
+
+    order.click_radiobutton_product_by_delivery("Курьер - бесплатно")
+    expect(page.get_by_label("Курьер - бесплатно")).to_be_checked()
+
+    time.sleep(2)
+
+    expect(page.get_by_label("Адрес доставки:")).to_be_visible()
+    expect(page.locator(".delivery-type.delivery-type-current label:has(span:has-text('Телефон:'))")).to_be_visible()
+    expect(page.locator(".delivery-type.delivery-type-current label:has(span:has-text('Дата и время доставки:'))")).to_be_visible()
+
+    order.click_button_making_order()
+    expect(page.locator(".error")).to_have_text("Неверно заполнено поле: Адрес доставки")
 
 
 
