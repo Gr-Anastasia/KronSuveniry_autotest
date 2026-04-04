81bdd98954672b24b2157ba30f733ce1302964f3
Nastya Gr <proekt28n@gmail.com>
2026-04-04 16:54:47 +0500

    Исправление 'багов' для ТК5 и верные проверки

--- tests/test_filter_price.py
+++ tests/test_filter_price.py
@@ -29,20 +29,20 @@ def test_04_filter(page: Page):
     expect(page.locator(".product-top")).to_have_count(count_product_before)
 
 def test_05_filter(page: Page):
-    """
-        НЕ РАБОТАЕТ! ОШИБКА БД, ПРАВИТЬ!
-    """
-
     office = SectionPage(page, "https://pumpenergy.ru/catalog/office")
     office.open()
 
-    # office.click_button_by_name('Сбросить фильтр')
     office.fill_filter_price_by_name('от ', '1234567891011121314151617181920')
+    page.keyboard.press("Enter")
     expect(page.locator('//label[contains(text(), "от ")]/input')).to_have_count(7)
-    time.sleep(10)
+    time.sleep(2)
 
     office.fill_filter_price_by_name('до ', 'HelloWord!, Привет всем')
+    page.keyboard.press("Enter")
     expect(page.locator('//label[contains(text(), "до ")]/input')).to_have_count(0)
-    time.sleep(10)
+    time.sleep(2)
+
     office.click_button_filter_by_name('Показать')
-    time.sleep(10)
+    count_product = page.locator(".product-top").count()
+    expect(page.locator('//*[@class="shop2-filter"]/span[@class="filter-result "]')).to_be_visible()
+    expect(page.locator("#filter-result")).to_have_text(f"{count_product}")
