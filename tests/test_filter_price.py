e11711c2f005532e60b8074d5ffb1852c62792a7
Nastya Gr <proekt28n@gmail.com>
2026-04-04 16:52:21 +0500

    Исправление 'багов' для ТК4 и верные проверки

--- tests/test_filter_price.py
+++ tests/test_filter_price.py
@@ -4,39 +4,29 @@ from pages.section_page import SectionPage
 from playwright.sync_api import expect, Page
 
 def test_04_filter(page: Page):
-    """
-        НЕ РАБОТАЕТ! ОШИБКА БД, ПРАВИТЬ!
-    """
-
     office = SectionPage(page, "https://pumpenergy.ru/catalog/office")
     office.open()
+    count_product_before = page.locator(".product-top").count()
 
-    # office.click_button_by_name('Сбросить фильтр')
     office.fill_filter_price_by_name('от ', '500')
-    # page.keyboard.press("Tab")
     page.keyboard.press("Enter")
-    # page.locator('input[name="s[price][min]"]').all()[1].blur()
     expect(page.locator('//label[contains(text(), "от ")]/input')).to_have_value('500')
     time.sleep(2)
 
     office.fill_filter_price_by_name('до ', '2000')
-    # page.keyboard.press("Tab")
     page.keyboard.press("Enter")
-    # page.locator('input[name="s[price][max]"]').all()[1].blur()
     expect(page.locator('//label[contains(text(), "до ")]/input')).to_have_value('2000')
     time.sleep(2)
 
     office.click_button_filter_by_name('Показать')
     time.sleep(2)
 
-    # count_product = page.locator(".product-top").count()
-    expect(page.locator('//*[@class="shop2-filter"]//span[@class="filter-result"]')).to_be_visible()
-    # expect(page.locator("#filter-result")).to_have_text(f"{count_product}")
-    # expect(page.locator(".product-top")).to_have_count("5")
+    count_product_after = page.locator(".product-top").count()
+    expect(page.locator('//*[@class="shop2-filter"]/span[@class="filter-result "]')).to_be_visible()
+    expect(page.locator("#filter-result")).to_have_text(f"{count_product_after}")
 
-    # office.click_button_filter_by_name('Сбросить фильтр')
-    # time.sleep(2)
-
+    office.click_button_filter_by_name('Сбросить фильтр')
+    expect(page.locator(".product-top")).to_have_count(count_product_before)
 
 def test_05_filter(page: Page):
     """
