640af41a8311605e59dd6638b3e6c619a176f410
Nastya Gr <proekt28n@gmail.com>
2026-04-04 17:32:16 +0500

    Исправление локатора в ТК8

--- tests/test_count_product.py
+++ tests/test_count_product.py
@@ -1,5 +1,3 @@
-import time
-
 from pages.section_page import SectionPage
 from playwright.sync_api import expect, Page
 
@@ -9,16 +7,16 @@ def test_08_count_product_card_mini(page: Page):
     office.open()
 
     office.click_button_add_count_input_by_title("Карта-флешка")
-    (expect(page.locator('//div[@class="product-item shop2-product-item"][.//a[text()="Карта-флешка"]]//input[@name="amount"]'))
-     .to_have_value("2"))
+    (expect(page.locator('.product-item shop2-product-item:has(a:has-text("Карта-флешка"))')
+            .locator('input[name="amount"]'))).to_have_value("2")
 
     office.click_button_remove_count_input_by_title("Карта-флешка")
-    (expect(page.locator('//div[@class="product-item shop2-product-item"][.//a[text()="Карта-флешка"]]//input[@name="amount"]'))
-     .to_have_value("1"))
+    (expect(page.locator('.product-item shop2-product-item:has(a:has-text("Карта-флешка"))')
+            .locator('input[name="amount"]'))).to_have_value("1")
 
     office.fill_input_count_by_title("Карта-флешка", "22")
-    (expect(page.locator('//div[@class="product-item shop2-product-item"][.//a[text()="Карта-флешка"]]//input[@name="amount"]'))
-     .to_have_value("22"))
+    (expect(page.locator('.product-item shop2-product-item:has(a:has-text("Карта-флешка"))')
+                         .locator('input[name="amount"]'))).to_have_value("22")
 
 
 
