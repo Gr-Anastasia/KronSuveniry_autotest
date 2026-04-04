dcb39302ef7e298677cd2c05ac925522f23afe8a
Nastya Gr <proekt28n@gmail.com>
2026-04-04 17:00:01 +0500

    Исправление ТК6, добавление рабочей првоерки для шага 3

--- tests/test_cart_product.py
+++ tests/test_cart_product.py
@@ -1,5 +1,4 @@
 import time
-
 from pages.product_page import ProductPage
 from pages.section_page import SectionPage
 from playwright.sync_api import expect, Page
@@ -12,12 +11,13 @@ def test_06_go_to_cart_product(page: Page):
     food.click_product_in_card_by_link("Тульский пряник")
     expect(page).to_have_url("https://pumpenergy.ru/catalog/pryanick-tula")
     expect(page.get_by_role("heading", name="Тульский пряник")).to_be_visible()
+    price = page.locator(".price-current > strong").inner_text()
     expect(page.locator(".price-current > strong")).to_have_text("700")
 
     product.click_button_product_by_name("Купить")
     expect(page.locator(".added-to-cart:has-text('Добавлено')")).to_be_visible()
-    time.sleep(2)
-    expect(page.locator("#cart_total")).to_have_text("700")
+    time.sleep(1)
+    expect(page.locator("#cart_total")).to_have_text(price)
     expect(page.locator("#cart_total_amount")).to_have_text("1")
 
 def test_07_count_cart_logo(page: Page):
